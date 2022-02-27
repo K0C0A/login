f4c4dcd0a8614f90181a17ae77efbadfd233a678
Christopher Balsillie <76265906+K0C0A@users.noreply.github.com>
2022-02-27 12:58:37 +0000

    Update App.js
    Added checkbox for showing password + a function for it

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+//Author: Christopher Balsillie
+//Legal Pythia Login Page Project
+
 import './App.css';
 import React, {useState} from "react";
 
@@ -17,29 +20,47 @@ function App() {
     });
   }
 
+  function showPassword(){
+    var x = document.getElementById("password");
+    if(x.type === "password"){
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
   return (
     <div className="App">
       <header>
+        <menu>
+
+        </menu>
       </header>
       <main>
         <form>
           <label>
-            Username: <input 
-                        type = "text" 
-                        name = "uname"
-                        onChange = {handleChange}
-                        value  = {state.uname}           
-                        />
+              <p>Username:</p> <br/>
+                        <input 
+                          type = "text" 
+                          name = "uname"
+                          onChange = {handleChange}
+                          value  = {state.uname}           
+                          />
           </label>
 <br/>
           <label>
-            Password: <input 
-                        type = "password" 
-                        name = "password"
-                        value  = {state.password}
-                        onChange = {handleChange}
-                        />
+              <p>Password:</p> <br/>
+                        <input
+                          id = "password" 
+                          type = "password" 
+                          name = "password"
+                          value  = {state.password}
+                          onChange = {handleChange}
+                          />
           </label>
+<br/>          
+          <p id = "checkbox">Show Password</p><input id = "checkbox" type = "checkbox" onClick={showPassword}/>
+          <button>Login</button>
         </form>   
       </main>
     </div>
